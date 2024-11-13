9379cc6d24d604101953a54141b258c358cadb15
Luiz Fernando <luizfsoliveira.lm@gmail.com>
2024-11-13 15:37:36 -0300

    Adicionar aqruivos da aula01

---
+++ aula01/exemplo_00.py
@@ -0,0 +1,5 @@
+# Crie um programa que o usuario digite o seu nome e retorna o numero de caracteres
+
+nome = input("Digite seu nome:")
+
+print(len(nome))
---
+++ aula01/exemplo_01.py
@@ -0,0 +1,2 @@
+# Crie um programa que o usuario digite dois numeros e retorne a soma
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
